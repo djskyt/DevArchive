853f11d3f5fa03188ccec17e184d548f294bbb0f
HyeonJin <jjsk953563@gmail.com>
2026-05-15 02:35:07 +0900

    레이아웃 적용 및  다크모드 클래스 기반 설정 추가

--- src/app/providers/router.jsx
+++ src/app/providers/router.jsx
@@ -1,20 +1,21 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from '../../shared/ui/Layout';
 import { HomePage } from '../../pages/home/page';
 import { BookmarksPage } from '../../pages/bookmarks/page';
 import { ArticlePage } from '../../pages/article/page';
 
 const router = createBrowserRouter([
   {
+    element: <Layout><HomePage /></Layout>,
     path: '/',
-    element: <HomePage />,
   },
   {
+    element: <Layout><BookmarksPage /></Layout>,
     path: '/bookmarks',
-    element: <BookmarksPage />,
   },
   {
+    element: <Layout><ArticlePage /></Layout>,
     path: '/articles/:id',
-    element: <ArticlePage />,
   },
 ]);
 
