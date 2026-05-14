1d1c2f5a6f35ba23af60872b83dd3077b1a73454
HyeonJin <jjsk953563@gmail.com>
2026-05-15 02:18:14 +0900

    홈 페이지 아티클 목록 및 검색 연결

--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,3 +1,23 @@
 export const HomePage = () => {
   return <div>Home</div>;
+};import { useSearch } from '../../features/search/model/useSearch';
+import { SearchBar } from '../../features/search/ui/SearchBar';
+import { ArticleCard } from '../../features/article/ui/ArticleCard';
+
+export const HomePage = () => {
+  const { query, setQuery, tag, setTag, articles, isPending, isError } = useSearch();
+
+  return (
+    <div>
+      <h1>Dev Archive</h1>
+      <SearchBar onSearch={setQuery} />
+      <div>
+        {isPending && <p>로딩 중...</p>}
+        {isError && <p>아티클을 불러올 수 없습니다.</p>}
+        {articles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
+    </div>
+  );
 };
