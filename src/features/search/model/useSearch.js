8231da3ca9c9cbf277ac173f3da070620a7ea17b
HyeonJin <jjsk953563@gmail.com>
2026-05-15 18:40:06 +0900

    다중태그지원으로 수정

--- src/features/search/model/useSearch.js
+++ src/features/search/model/useSearch.js
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import { useArticles } from '../../../features/article/model/useArticles';
 import { useDebounce } from '../../../shared/hooks/useDebounce';
-import { useState } from 'react';
 
 export const useSearch = () => {
   const [query, setQuery] = useState('');
-  const [tag, setTag] = useState('');
+  const [tags, setTags] = useState([]);
   const debouncedQuery = useDebounce(query);
+
+  const toggleTag = (tag) => {
+    setTags((prev) =>
+      prev.includes(tag) ? prev.filter((t) => t !== tag) : [...prev, tag]
+    );
+  };
+
+  const clearTags = () => setTags([]);
 
   const { data: articles = [], isPending, isError } = useArticles({
     q: debouncedQuery,
-    tag,
+    tags,
   });
 
-  return { query, setQuery, tag, setTag, articles, isPending, isError };
+  return { query, setQuery, tags, toggleTag, clearTags, articles, isPending, isError };
 };
